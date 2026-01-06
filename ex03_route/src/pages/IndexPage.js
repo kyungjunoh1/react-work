be400f8c595a5bdc95510e62d5f444388f1aea20
kyungjunoh <orudwns11@naver.com>
2026-01-06 16:49:47 +0900

    인덱스페이지

--- ex03_route/src/pages/IndexPage.js
+++ ex03_route/src/pages/IndexPage.js
@@ -1,6 +1,17 @@
+import { useEffect, useState } from "react";
+
 function IndexPage(){
+    const [num, setNum] = useState(0)
+    const [num2, setNum2] = useState(0)
+    useEffect(()=>{
+         console.log("useEffect 실행 :", num)
+    },[num2,num])
+    const onClick = () => {setNum(num + 1); }
+    const onClick2 = () => {setNum2(num2 + 1); }
     return (<>
-       indexpage
+       num : {num}, num2 : {num2} <hr />
+       <button onClick={onClick}>클릭</button>
+       <button onClick={onClick2}>클릭22</button>
     </>)
 
 }
