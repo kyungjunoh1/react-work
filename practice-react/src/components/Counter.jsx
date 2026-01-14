e98352b8b9844c40be2da4c67569a3e838e0bcb9
kyungjunoh <orudwns11@naver.com>
2026-01-14 18:50:45 +0900

    practice

--- practice-react/src/components/Counter.jsx
+++ practice-react/src/components/Counter.jsx
@@ -7,9 +7,26 @@ const Counter = () => {
   return (
     <div>
       <div>Counter</div>
-      <button onClick={handleClick()}>클릭</button>
+      <button onClick={handleClick}>클릭</button>
     </div>
   );
 };
+const user = {name : "철수", age: 20 };
+
+const updateAge = (u) => {
+  return {...u, age:21};
+};
+
+const user2 = updateAge(user);
+
+const nums = [1, 2, 3];
+
+const result = nums.map(n => {
+  if(n%2===0){
+    return n*10;
+  }
+  return n;
+});
+console.log("nums : ",result)
 
 export default Counter;
